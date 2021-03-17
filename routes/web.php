135d70a42a98f835570fba3e59df40eefcfae781
minhthien10c2 <55235087+minhthien10c2@users.noreply.github.com>
2021-03-17 21:55:44 +0700

    First setup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\ImageUploadController::class, 'index'])->name('home');
+Route::post('/upload', [App\Http\Controllers\ImageUploadController::class, 'store'])->name('upload');
+
